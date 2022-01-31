32131e0ee16d11bd0092b8e59c8a55c398f899af
nickbanders <nickbanders@gmail.com>
2022-01-30 21:26:17 -0600

    adding final project code

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+const savedNotes = require("../../db/db.json");
+const fs = require("fs");
+const { nanoid } = require("nanoid");
+
+router.get("/notes", (req, res) => {
+  let results = savedNotes;
+  console.log("got new note");
+  res.json(results);
+});
+
+router.get("/db", (req, res) => {
+  console.log("database");
+  res.json(savedNotes);
+});
+
+router.post("/notes", (req, res) => {
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = { id: nanoid(), title, text };
+    res.send("done");
+    savedNotes.push(newNote);
+
+    fs.writeFile("db/db.json", JSON.stringify(savedNotes), (err) =>
+      err ? console.error(err) : console.log("note written")
+    );
+
+    // res.json(newNote);
+  }
+});
+
+router.delete("/notes/:id", (req, res) => {
+  const id = req.params.id;
+  let delNote = savedNotes.filter((curNote) => {
+    return curNote.id != id;
+  });
+
+  fs.writeFileSync("db/db.json", JSON.stringify(delNote));
+  res.json(delNote);
+});
+
+module.exports = router;
